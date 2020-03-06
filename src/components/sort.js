fbfaff9bb2580a72bd7e9bd95864502f3d2f0795
MMarek <mildnermarek@gmail.com>
2020-03-06 11:26:03 +0100

    sort correction example & pagination example

--- src/components/sort.js
+++ src/components/sort.js
@@ -26,46 +26,16 @@ console.log(bubbleSort([64, 240, 8, 0, 64, -1, -4]));
 console.log(bubbleSort([56, -140, -8, 0, 83, -42, 5, -1.6]));
 
 
-// IIFE - Immediately-invoked function expression =  funkcja samowykonująca się w JavaScript.
+// wzór na sortowanie rosnące (b - a = malejące)
 
-const shopModule = (function () {
-    const _products = [];
 
-    const _countNewPrice = (price) => {
-        return price + (price * 0.5) - 0.1;
-    };
+const arrayToSort = [135, 24, 6, 46, 8, 246, 4];
 
-    const addProduct = (product) => {
-        const newPrice = _countNewPrice(product.price);
-        const newProduct = Object.assign({}, product, {price: newPrice});
-        _products.push(newProduct);
-    };
+function sortArray(array) {
+    array.sort(function (a, b) {
+        return a - b;
+    });
+    return array;
+}
 
-    const showProducts = () => {
-        console.log(_products);
-    };
-
-    return {
-        addProduct,
-        showProducts
-    }
-
-})();
-
-const product1 = {
-    id: 1,
-    name: 'słuchawki',
-    price: 200
-};
-
-const product2 = {
-    id: 2,
-    name: 'buty',
-    price: 300
-};
-
-shopModule.addProduct(product1);
-shopModule.addProduct(product2);
-shopModule.showProducts();
-
-console.log(product1, product2);
+console.log(sortArray(arrayToSort));
