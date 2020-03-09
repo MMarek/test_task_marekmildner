ce4e40b6b729d3a59bab6ba667b5f6fc70fa7764
MMarek <mildnermarek@gmail.com>
2020-03-09 11:49:07 +0100

    pagination/style

--- src/components/error.js
+++ src/components/error.js
@@ -52,3 +52,172 @@ fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
         }
     })
     .catch(err => console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, {Component} from "react";
+
+class Main extends Component {
+    state = {
+        data: [],
+        sort: null,
+
+        visibleType: 0,
+        allButtons: 5,
+        itemsPerPage: 5,
+        currentPage: 5,
+        active: '',
+    };
+
+
+
+
+    changeType = (e, i) => {
+
+        let buttonNumber;
+        const typeNumber = parseInt(this.state.data[i].items.length, 10);
+
+        if (typeNumber !== 5 && typeNumber % 5 === 0) {
+            buttonNumber = typeNumber / 5;
+        } else if (typeNumber === 5) {
+            buttonNumber = 0;
+        }
+
+        this.setState({
+            visibleType: i,
+            allButtons: buttonNumber,
+            currentPage: 1,
+            active: i,
+        });
+    };
+
+    changeSite = (e, i) => {
+
+        this.setState({
+            currentPage: i,
+            active: i,
+        });
+    };
+
+    showButtons = () => {
+        let buttons = [];
+        for (let i = 1; i <= this.state.allButtons ; i++) {
+            buttons.push(
+                <button key={i}
+                        onClick={(e) => this.changeSite(e,i)}
+                        className={`buttonDisplay ${this.state.currentPage === i ? 'active' : ''}`}>{i}</button>
+            );
+        }
+        return buttons;
+    };
+
+
+
+
+
+    handleClickId = () => {
+        if (this.state.sort === null) {
+            this.setState({sort: 1})
+        } else if (this.state.sort === 1) {
+            this.setState({sort: 2})
+        } else if (this.state.sort === 2) {
+            this.setState({sort: 1})
+        }
+    };
+
+    componentDidMount() {
+        fetch('https://recruitment.hal.skygate.io/companies')
+            .then(response => response.json())
+            // .then(response => {console.log(response)})
+            .then(data => this.setState({data}))
+            .catch(err => err.log(err));
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(this.state.sort);
+    }
+
+
+    render() {
+
+
+
+
+
+
+        let buttonList;
+        buttonList = this.showButtons();
+        const {currentPage, itemsPerPage, visibleType} = this.state;
+        const itemArray = this.state.data.items;
+        const indexLast = currentPage * itemsPerPage;
+        const indexFirst = indexLast - itemsPerPage;
+        // const curren = itemArray.slice(indexFirst, indexLast);
+
+
+
+
+
+
+        const {data} = this.state;
+
+        return (
+            <CompaniesItem data={data} click={this.handleClickId}/>
+        )
+    }
+}
+
+
+class CompaniesItem extends Component {
+    render() {
+        let data = this.props.data;
+        // const {id, name, city} = this.props.data[32];
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th onClick={this.props.click}>ID</th>
+                    <th>NAME</th>
+                    <th>CITY</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map(item =>
+                    <tr key={item.id}>
+                        <th>{item.id}</th>
+                        <th>{item.name}</th>
+                        <th>{item.city}</th>
+                    </tr>)}
+                </tbody>
+
+            </table>
+
+
+
+
+
+
+
+
+
+        );
+    }
+}
+
+
+export default Main;
--- src/tasc/main.js
+++ src/tasc/main.js
@@ -1,17 +1,30 @@
 import React, {Component} from "react";
 
 class Main extends Component {
+
+
     state = {
         data: [],
-        sort: null
+        sort: null,
+
+        currentPage: 1,
+        namesPerPage: 20
     };
+
+
+    handleClick = (event, i) => {
+        this.setState({
+            currentPage: i
+        })
+    };
+
 
     handleClickId = () => {
         if (this.state.sort === null) {
             this.setState({sort: 1})
-        }else if(this.state.sort === 1) {
+        } else if (this.state.sort === 1) {
             this.setState({sort: 2})
-        }else if (this.state.sort === 2) {
+        } else if (this.state.sort === 2) {
             this.setState({sort: 1})
         }
     };
@@ -30,10 +43,65 @@ class Main extends Component {
 
 
     render() {
-        const {data} = this.state;
+
+        // const {data} = this.state;
+
+
+        const {data, currentPage, namesPerPage} = this.state; // destrukturyzacja do paginacji
+        const indexOfLast = currentPage * namesPerPage;
+        const indexOfFirst = indexOfLast - namesPerPage;
+        const currentData = data.slice(indexOfFirst, indexOfLast);
+
+        const elements = currentData.map((name, i) => {
+            return (
+
+                <tr key={name.id}>
+                    <th>{name.id}</th>
+                    <th>{name.name}</th>
+                    <th>{name.city}</th>
+                </tr>
+            )
+        });
+
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(data.length / namesPerPage); i++) {
+            {
+                const element = <button key={i}
+                                    onClick={e => this.handleClick(e, i)}
+                                    className={this.state.currentPage === i ? 'active' : ''}
+                >
+                    {i} </button>;
+                pageNumbers.push(element)
+            }
+        }
+
+
+
+
 
         return (
-            <CompaniesItem data={data} click={this.handleClickId}/>
+            <>
+                {/*<CompaniesItem data={data} click={this.handleClickId}/>*/}
+                <section>
+                    {/*<ul className='names'> {elements} </ul>*/}
+
+                    <table>
+                        <thead>
+                        <tr>
+                            <th onClick={this.props.click}>ID</th>
+                            <th>NAME</th>
+                            <th>CITY</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {elements}
+                        </tbody>
+
+                    </table>
+
+                    <ul className='numbers'> {pageNumbers} </ul>
+                </section>
+            </>
         )
     }
 }
@@ -41,10 +109,12 @@ class Main extends Component {
 
 class CompaniesItem extends Component {
     render() {
+
         let data = this.props.data;
         // const {id, name, city} = this.props.data[32];
         return (
-            <table>
+            <>
+                <table>
                     <thead>
                     <tr>
                         <th onClick={this.props.click}>ID</th>
@@ -52,15 +122,19 @@ class CompaniesItem extends Component {
                         <th>CITY</th>
                     </tr>
                     </thead>
-                <tbody>
-                {data.map(item =>
-                    <tr key={item.id}>
-                        <th>{item.id}</th>
-                        <th>{item.name}</th>
-                        <th>{item.city}</th>
-                    </tr>)}
-                </tbody>
-            </table>
+                    <tbody>
+                    {data.map(item =>
+                        <tr key={item.id}>
+                            <th>{item.id}</th>
+                            <th>{item.name}</th>
+                            <th>{item.city}</th>
+                        </tr>)}
+                    </tbody>
+                </table>
+
+            </>
+
+
         );
     }
 }
