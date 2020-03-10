cca2f0fac4632f76fdd5abb48778d49033f958ee
MMarek <mildnermarek@gmail.com>
2020-03-10 11:33:16 +0100

    Updates

---
+++ precache-manifest.6003dc5eccae8104f42ede120d3e27f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424be21285d1adc751c33f252243660d",
+    "url": "/test_task_marekmildner/index.html"
+  },
+  {
+    "revision": "35f19ae721e3f53bdaf8",
+    "url": "/test_task_marekmildner/static/css/main.bb5e6b1c.chunk.css"
+  },
+  {
+    "revision": "e856cca7b5b9ce0c8175",
+    "url": "/test_task_marekmildner/static/js/2.2b022208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test_task_marekmildner/static/js/2.2b022208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f19ae721e3f53bdaf8",
+    "url": "/test_task_marekmildner/static/js/main.679f6ef5.chunk.js"
+  },
+  {
+    "revision": "ac4336d9a7b2179804aa",
+    "url": "/test_task_marekmildner/static/js/runtime-main.196e8bc6.js"
+  }
+]);
